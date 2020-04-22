2a18aca72298e69a70ec907bd9802cab20c93ea5
ShunyaSuzuki <anyushu2017@gmail.com>
2020-04-22 13:13:44 +0900

    update

--- footer.php
+++ footer.php
@@ -1,12 +1,40 @@
-<?php
-$home = esc_url(home_url());
-$wp_url = get_template_directory_uri(); ?>
 </main>
-<!-- メインコンテンツ終了 -->
-<!-- フッター -->
-<footer>
+
+<?php get_template_part('templates/contact'); ?>
+
+<footer class="footer sec">
+<div class="container">
+<div class="row justify-content-between footer__inner">
+<div class="col-md-4">
+<p class="footer__inner__logo"><a href="<?php e_home_url(); ?>"><img class="img-fluid" src="<?php e_img_url(); ?>/head_logo.png" alt="<?php bloginfo('name'); ?>"></a></p>
+<p class="small mb-0 footer__inner__address">〒550-0015
+<br>大阪府大阪市⻄区南堀江3丁目15−22
+<br>TEL 06-6531-9200</p>
+</div>
+<div class="col-md-6">
+<div class="text-center">
+<a class="btn btn-primary" href="<?php e_home_url(); ?>">
+<span class="btn-primary--point">＼ 24時間受付中 ／</span>
+<span class="btn-primary--txt">無料査定・売却相談はこちら</span>
+</a>
+</div>
+<ul class="footer__inner__link">
+<li><a href="<?php e_home_url(); ?>">トップページ</a></li>
+<li><a href="<?php e_home_url(); ?>">サービス内容</a></li>
+<li><a href="<?php e_home_url(); ?>">買取実績</a></li>
+<li><a href="<?php e_home_url(); ?>">サービスの流れ</a></li>
+<li><a href="<?php e_home_url(); ?>">お客様の声</a></li>
+<li><a href="<?php e_home_url(); ?>">ブログ</a></li>
+<li><a href="<?php e_home_url(); ?>">会社案内</a></li>
+</ul>
+</div>
+</div>
+</div>
+<!-- container -->
 </footer>
-<!-- フッター終了 -->
+<!-- footer -->
+<small class="d-block text-center p-2 border-top">©2020 空き家買取屋さん</small>
+<a class="to-top" href="#"><img class="img-fluid" src="<?php e_img_url(); ?>/to_top.png" alt="トップへ"></a>
 <?php wp_footer(); ?>
 </body>
 </html>
---
+++ templates/contact.php
@@ -0,0 +1,21 @@
+<div class="contact">
+<div class="container position-relative">
+<p class="contact__h">
+<img src="<?php e_img_url(); ?>/contact_h.png" alt="どんなに些細な事でもお気軽にお問い合わせください。">
+</p>
+<div class="contact__link">
+<a href="">
+<img src="<?php e_img_url(); ?>/btn_assessment.png" alt="">
+</a>
+<a class="mx-3" href="">
+<img src="<?php e_img_url(); ?>/btn_buying.png" alt="">
+</a>
+<a href="">
+<img src="<?php e_img_url(); ?>/contact_tel.png" alt="">
+</a>
+</div>
+<img class="contact--bg" src="<?php e_img_url(); ?>/bg_contact_text.png" alt="背景">
+</div>
+<!-- container -->
+</div>
+<!-- contact -->
